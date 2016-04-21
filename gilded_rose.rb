6133c17a788b14115cb231078de4e783ee5fd4b6
Gregory Brown <gregory.t.brown@gmail.com>
2016-04-21 15:02:01 -0400

    Add normal item rules

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,8 +4,25 @@ class GildedRose
     @items = items
   end
 
+  def normal_item?(item)
+    item.name != "Aged Brie" &&
+    item.name != "Backstage passes to a TAFKAL80ETC concert" &&
+    item.quality > 0 && item.sell_in > 0
+  end
+
+  def update_normal_item(item)
+    return false unless normal_item?(item)
+
+    item.quality -= 1
+    item.sell_in -= 1
+
+    true
+  end
+
+
   def update_quality()
     @items.each do |item|
+      update_normal_item(item) ||
       _update_quality_original(item)
     end
   end
